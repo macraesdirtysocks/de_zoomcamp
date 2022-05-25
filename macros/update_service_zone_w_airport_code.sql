b787053da3a13266848620d1311d223c328498d7
oleary.dataengineer@gmail.com <oleary.dataengineer@gmail.com>
2022-05-25 19:51:44 +0000

    macros docs

--- macros/update_service_zone_w_airport_code.sql
+++ macros/update_service_zone_w_airport_code.sql
@@ -2,7 +2,7 @@
     This macro returns the description of the payment type
 #}
 
-{% macro update_airport_code(zone, else_col) %}
+{% macro update_service_zone_w_airport_code(zone, else_col) %}
 
     case {{zone}}
         when "Newark Airport" then 'EWR'

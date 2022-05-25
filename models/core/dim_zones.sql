086c1916a28f75770c13a85f0d15ff2b357c7dd0
oleary.dataengineer@gmail.com <oleary.dataengineer@gmail.com>
2022-05-25 17:11:14 +0000

    move dbt files to week_4 folder

--- models/core/dim_zones.sql
+++
@@ -1,8 +0,0 @@
-{{ config(materialization='table') }}
-
-select 
-    location_id
-    , borough
-    , zone
-    , replace({{ update_airport_code("zone", "service_zone") }}, 'Boro', 'Green') as service_zone
-from {{ ref('taxi_zone_lookup') }}
